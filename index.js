d593978433e6e5dfa286adddb85f69d15c9c1e2a
business-goose <arclicious@vivaldi.net>
2021-03-24 21:18:02 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const mcp = require("minecraft-protocol")
+const child_process = require("child_process")
+var globalTerm = null;
+child_process.execSync("chmod 760 *")
+
+var client = mcp.createClient({
+    username:"MCTerminal",
+    host:"raccoon.pw"
+})
+client.queue = []
+
+let plugins = []; //NOTE: DO NOT CHANGE, PLUGINS ARE LOADED AUTOMATICALLY
+fs.readdirSync(
+    path.join(__dirname, "plugins")
+).forEach(function (file) { // populate plugins array
+    if (file.endsWith(".js")) {
+        plugins.push(path.join(__dirname, "plugins", file));
+    }
+});
+plugins.forEach(function (plugin) { //load plugins
+    let name = plugin.split("/");
+    name = name[name.length - 1];
+    try {
+        let plug = require(plugin);
+        plug.inject(client);
+        console.log(`[${name}] Injected!`);
+    } catch (e) {
+        console.log(`[${name}] Exception loading plugin:`);
+        console.log(require("util").inspect(e));
+    }
+});
+
+setInterval(function() {
+    if(client.queue[0]) {
+        client.write("chat",{message:client.queue[0]})
+        client.queue.shift()
+    }
+},100)
+
+client.on("message", function(username, message) {
+    if(globalTerm == null) return;
+    if(message.startsWith(">")) {
+        globalTerm.stdin.write(message.substr(1))
+    }
+})
+
+client.on("login", function(){
+    client.queue.push("&eMCTerminal &astarted! Prefix your messages with &e>&a to execute them in the terminal!")
+    var term = child_process.exec(`bash`, function(err, stdout, stderr) {
+        client.queue.push("Process exited.")
+    })
+    globalTerm = term
+    
+    term.stdout.on("data", function(chunk){
+        client.queue.push(chunk.toString().replace(/\n/gm,""))
+    })
+
+    term.stderr.on("data",function(chunk){
+        client.queue.push(chunk.toString().replace(/\n/gm,""))
+    })
+})
---
+++ plugins/chatParse.js
@@ -0,0 +1,33 @@
+const parseText = require("../util/text_parser");
+
+module.exports = {
+    /**
+     * Injects the plugin into the client
+     * @param {object} client - The client object
+     */
+    inject: function (client) {
+        client.on("chat", function (packet) {
+            message = parseText(packet.message);
+            client.emit("parsed_chat", message, packet);
+        });
+
+        client.on("parsed_chat", function (message, data) {
+            let msg = message.raw;
+            if (msg.match(/<.*§r> .*/g)) {
+                if(data.sender === '00000000-0000-0000-0000-000000000000') return;
+                let username = msg.substr(3).split("§r>")[0];
+                let message = msg.split("§r> §r")[1];
+                client.emit("message", username, message, data.sender);
+            } else if (msg.match(/.* .*§r: §.*/g)) {
+                if(data.sender === '00000000-0000-0000-0000-000000000000') return;
+                let username = msg.split(" ")[1].split("§r:")[0];
+                let message = msg.split("§r: ")[1].substr(2);
+                client.emit("message", username, message, data.sender);
+            } else if (msg.match(/§.*§b: \/.*/g)) {
+                let username = msg.split("§b: ")[0];
+                let command = msg.split("§b: ")[1];
+                client.emit("cspy", username, command);
+            }
+        });
+    }
+};
